53ec30c6f1118c870f468688e785b73b0b0d52ef
亗 ʜᴀʀɪ 亗 | தமிழன் | 🇮🇳 <151769010+HarixTGX@users.noreply.github.com>
2024-11-07 14:47:07 +0530

    fix

---
+++ info.py
@@ -0,0 +1,12 @@
+import re
+from os import environ
+id_pattern = re.compile(r'^.\d+$')
+
+# Bot information
+SESSION = environ.get('SESSION', 'SinglePageMessageCompilerBot')
+API_ID = int(environ.get('API_ID', '10261086'))
+API_HASH = environ.get('API_HASH', '9195dc0591fbdb22b5711bcd1f437dab')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7849587328:AAEQ02rnEkqMoxS2D28ENHKwTMKBTzA-Igg')
+PORT = environ.get('PORT', '8080')
+
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1426588906').split()]
