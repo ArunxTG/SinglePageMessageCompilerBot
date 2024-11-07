a5b5bdce511b60ef8e5906f7c1b70b6a9e1adeb7
亗 ʜᴀʀɪ 亗 | தமிழன் | 🇮🇳 <151769010+HarixTGX@users.noreply.github.com>
2024-11-07 16:27:52 +0530

    fix

--- info.py
+++ info.py
@@ -9,4 +9,4 @@ API_HASH = environ.get('API_HASH', '9195dc0591fbdb22b5711bcd1f437dab')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7849587328:AAEQ02rnEkqMoxS2D28ENHKwTMKBTzA-Igg')
 PORT = environ.get('PORT', '8080')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1426588906').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1426588906, 556766661').split()]
