cd5d8cf8ece4ba222cfd15f1f3b4604011d2612a
ArunxTG <jarvistg65@gmail.com>
2024-11-09 05:00:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,4 +9,13 @@ API_HASH = environ.get('API_HASH', '9195dc0591fbdb22b5711bcd1f437dab')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6874843676:AAErl1i5vtb0yEmdsYoGGvXsOtM9t5K8yn8')
 PORT = environ.get('PORT', '8080')
 
+# Scraper Settings
+BASE_URL = environ.get('BASE_URL', 'https://www.1tamilmv.wf')
+CHECK_INTERVAL = int(environ.get('CHECK_INTERVAL', '300'))  # 5 minutes by default
+CHANNEL_ID = int(environ.get('CHANNEL_ID', '-1001919732447'))
+
+# MongoDB Settings
+MONGO_URI = environ.get('MONGO_URI', 'mongodb+srv://KingSrilanga:KingSrilanga10@cluster0.w6gdp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1426588906, 556766661').split()]
