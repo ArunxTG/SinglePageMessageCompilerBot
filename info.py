994bb1485a4795d6b10a1cde293fe244efee4b95
ArunxTG <jarvistg65@gmail.com>
2024-11-09 05:40:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,7 +6,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'SinglePageMessageCompilerBot')
 API_ID = int(environ.get('API_ID', '10261086'))
 API_HASH = environ.get('API_HASH', '9195dc0591fbdb22b5711bcd1f437dab')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6874843676:AAErl1i5vtb0yEmdsYoGGvXsOtM9t5K8yn8')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5890914076:AAGifAR3sQ1Ayfv_kdPHyWfY16oZgAfA8BM')
 PORT = environ.get('PORT', '8080')
 
 # Scraper Settings
